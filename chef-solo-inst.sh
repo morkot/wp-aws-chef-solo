db8777db7fe7e80dcc0e5a830b67d1afc7cd4947
morkot <melekhovskiy@gmail.com>
2012-10-19 18:17:31 +0300

    add logging

--- chef-solo-inst.sh
+++ chef-solo-inst.sh
@@ -1,11 +1,17 @@
 #!/bin/bash -x
 
+#writing logs
+LOGS="/root/post-install.$(date -I)"
+exec > $LOGS 2>&1
+
 SOLODIR="/var/chef-solo"
 CFGDIR="$SOLODIR/wp-aws-chef-solo"
-#COOKBOOKDIR="$SOLODIR/cookbooks"
 
 # install Chef 
 rpm -ivh http://opscode-omnitruck-release.s3.amazonaws.com/el/6/x86_64/chef-10.14.4-2.el6.x86_64.rpm
+
+# install git
+yum -y install git
 
 #create dir and download cookbooks
 mkdir "$SOLODIR"
